8530b9686637666458e68e3c59a4803602d05ce2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-09 21:15:41 +0530

    Add: stone-game - submission-2

---
+++ Data Structures & Algorithms/stone-game/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        total = sum(piles)
+        mem = {}
+
+        def helper(start, end):
+            if (start, end) in mem:
+                return mem[(start, end)]
+            if start > end:
+                return 0
+            
+            ans = max( helper(start+1, end) + piles[start], 
+                        helper(start, end-1) + piles[end] )
+            
+            mem[(start, end)] = ans
+            return ans
+        
+        alice = helper(0,len(piles)-1)
+        bob = total - alice
+
+        return alice > bob
