831632489e244738d6fb0a1caae96cb416c4baf1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-16 20:40:25 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        mem = {}
+        coins = sorted(coins, reverse=True)
+        
+        def dfs(cnt,sum):
+            if (cnt,sum) in mem:
+                return mem[(cnt,sum)]
+
+            if sum == amount:
+                return cnt            
+
+            min_cnt = pow(2,31)
+            for coin in coins:
+                if sum+coin > amount:
+                    continue
+                min_cnt = min(dfs(cnt+1,sum+coin),min_cnt)
+            
+            mem[(cnt,sum)] = min_cnt
+            return min_cnt
+        
+        ans = dfs(0,0)
+        if ans == pow(2,31):
+            return -1
+        return ans
