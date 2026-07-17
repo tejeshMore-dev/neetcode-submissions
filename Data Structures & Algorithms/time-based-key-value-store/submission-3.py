6d701c782ac7c66cbaec15e862306c0c20e17b1f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-17 18:23:17 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class TimeMap:
+
+    def __init__(self):
+        self.k_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.k_map[key].append((timestamp,value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.k_map[key]
+        n = len(arr)
+        if n == 0:
+            return ""
+        
+        L = 0
+        R = n-1
+        ans = ""
+
+        while L<=R:
+            mid = L + (R-L)//2
+
+            if arr[mid][0] == timestamp:
+                return arr[mid][1]
+
+            if arr[mid][0] > timestamp:
+                R = mid-1
+            else:
+                ans = arr[mid][1]
+                L = mid+1
+        
+        return ans
