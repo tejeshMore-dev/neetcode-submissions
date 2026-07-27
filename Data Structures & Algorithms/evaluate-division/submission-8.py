b5d9cabc280c67fb0868c4aac8df258a43db3db2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2026-07-27 18:37:18 +0530

    Add: evaluate-division - submission-8

---
+++ Data Structures & Algorithms/evaluate-division/submission-8.py
@@ -0,0 +1,49 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+
+        for i in range(len(equations)):
+            a, b = equations[i]
+            v = values[i]
+
+            graph[a].append((b, v))
+            graph[b].append((a, 1/v))
+        
+        def helper(a, b):
+            queue = deque([])
+            queue.append((a, 1))
+            ans = 1
+            is_found = False
+            visited = set()
+            visited.add(a)
+            
+            while queue:
+                node, ans = queue.popleft()
+                if node == b:
+                    # print(node, ans)
+                    is_found = True
+                    ans = ans
+                    break
+
+                for nei, w in graph[node]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        # ans *= w
+                        queue.append((nei, ans * w))
+
+
+            if is_found:
+                return ans
+
+            return -1
+
+        ans = []
+        for a, b in queries:
+            if a not in graph or b not in graph:
+                ans.append(-1)
+            else:
+                ans.append(helper(a,b))
+            
+        return ans
